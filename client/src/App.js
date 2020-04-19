b3b1b7103b5d2522f4398db1bb253320cf1228c3
Dmitry Dyomin <dma01x@gmail.com>
2020-04-20 00:25:10 +0300

    Changin title

--- client/src/App.js
+++ client/src/App.js
@@ -12,9 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
+            <p>Fib Calculator</p>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
